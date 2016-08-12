5acf53c9949b90ef619ad8b7b7a8934cc9eba813
dkwasny <dkwasny67@gmail.com>
2016-08-12 20:35:14 +0000

    Finished chapter 2

--- src/main/scala/net/kwas/impatient/Driver.scala
+++ src/main/scala/net/kwas/impatient/Driver.scala
@@ -3,7 +3,10 @@ object Driver extends App {
 	import collection.immutable.List
 	import net.kwas.impatient._
 	
-	val apps = List(ch1.Exercises)
+	val apps = List(
+		ch1.Exercises,
+		ch2.Exercises
+	)
 	
 	for (app <- apps) {
 		println(s"\nRunning ${app.getClass}")
