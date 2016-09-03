13a823043dfbdddc55c631ec6ea77d67e12b95fd
dkwasny <dkwasny67@gmail.com>
2016-09-03 18:00:27 +0000

    Changing to 2 spaces per Scala's style guide

--- src/main/scala/net/kwas/impatient/ch1/Exercises.scala
+++ src/main/scala/net/kwas/impatient/ch1/Exercises.scala
@@ -1,34 +1,34 @@
 package net.kwas.impatient.ch1
 object Exercises extends App {
-	import math.sqrt
-	import math.pow
-	import math.BigInt
-	import math.BigInt.probablePrime
-	import util.Random
+  import math.sqrt
+  import math.pow
+  import math.BigInt
+  import math.BigInt.probablePrime
+  import util.Random
 
-	val notReallyThree = pow(sqrt(3),2)
-	println(s"Not Quite Three: ${notReallyThree}")
+  val notReallyThree = pow(sqrt(3),2)
+  println(s"Not Quite Three: ${notReallyThree}")
 
-	println("crazy" * 3)
+  println("crazy" * 3)
 
-	val normalExponent = pow(2, 1024)
-	println(s"Normal Exponent: ${normalExponent}")
+  val normalExponent = pow(2, 1024)
+  println(s"Normal Exponent: ${normalExponent}")
 
-	val bigExponent = BigInt(2) pow 1024
-	println(s"Big Exponent: ${bigExponent}")
+  val bigExponent = BigInt(2) pow 1024
+  println(s"Big Exponent: ${bigExponent}")
 
-	val myPrime = probablePrime(100, Random)
-	println(s"My Prime: ${myPrime}")
+  val myPrime = probablePrime(100, Random)
+  println(s"My Prime: ${myPrime}")
 
-	val randomString = BigInt(50, Random) toString 36
-	println(s"Random String: ${randomString}")
-	
-	val myString = "gahnachies"
-	val firstChar = myString(0)
-	val otherFirstChar = myString take 1
-	val lastChar = myString(myString.length - 1)
-	val otherLastChar = myString takeRight 1
-	println(s"My String: ${myString}")
-	println(s"First Char: ${firstChar} and ${otherFirstChar}")
-	println(s"Last Char: ${lastChar} and ${otherLastChar}")
+  val randomString = BigInt(50, Random) toString 36
+  println(s"Random String: ${randomString}")
+  
+  val myString = "gahnachies"
+  val firstChar = myString(0)
+  val otherFirstChar = myString take 1
+  val lastChar = myString(myString.length - 1)
+  val otherLastChar = myString takeRight 1
+  println(s"My String: ${myString}")
+  println(s"First Char: ${firstChar} and ${otherFirstChar}")
+  println(s"Last Char: ${lastChar} and ${otherLastChar}")
 }
--- src/main/scala/net/kwas/impatient/ch2/Exercises.scala
+++ src/main/scala/net/kwas/impatient/ch2/Exercises.scala
@@ -1,79 +1,79 @@
 package net.kwas.impatient.ch2
 object Exercises extends App {
-	import math.pow
+  import math.pow
 
-	def signum(x: Int) = { if (x > 0) 1 else if (x < 0) -1 else 0 }
-	val sigs = for (i <- -3 to 3) yield signum(i)
-	println(s"Sigs: ${sigs}")
+  def signum(x: Int) = { if (x > 0) 1 else if (x < 0) -1 else 0 }
+  val sigs = for (i <- -3 to 3) yield signum(i)
+  println(s"Sigs: ${sigs}")
 
-	var y = -1
-	val x = y = 1
-	println(s"Unit: ${x}")
+  var y = -1
+  val x = y = 1
+  println(s"Unit: ${x}")
 
-	println("For Loop:")
-	for (i <- 10 to 0 by -1) {
-		print(i)
-	}
-	println
+  println("For Loop:")
+  for (i <- 10 to 0 by -1) {
+    print(i)
+  }
+  println
 
-	println("Foreach call:")
-	(10 to 0 by -1).foreach(print)
-	println
+  println("Foreach call:")
+  (10 to 0 by -1).foreach(print)
+  println
 
-	def countdown(x: Int) {
-		for (i <- x to 0 by -1) {
-			print(i + " ")
-		}
-		println
-	}
-	print("Countdown: ")
-	val unit = countdown(10)
-	println(s"Return Type of Countdown: ${unit}")
+  def countdown(x: Int) {
+    for (i <- x to 0 by -1) {
+      print(i + " ")
+    }
+    println
+  }
+  print("Countdown: ")
+  val unit = countdown(10)
+  println(s"Return Type of Countdown: ${unit}")
 
-	def product(input: String) = {
-		var retVal = 1
-		for (x <- input) {
-			// x is a Char, but is implicitly converted to Int
-			retVal *= x
-		}
-		retVal
-	}
-	val helloProduct = product("Hello")
-	println(s"Hello Product: ${helloProduct}")
+  def product(input: String) = {
+    var retVal = 1
+    for (x <- input) {
+      // x is a Char, but is implicitly converted to Int
+      retVal *= x
+    }
+    retVal
+  }
+  val helloProduct = product("Hello")
+  println(s"Hello Product: ${helloProduct}")
 
-	// Without mapping to Ints the multiplication happens as chars, which overflows
-	val easyProduct = "Hello".map(Char.char2int).product
-	println(s"Easy Product: ${easyProduct}")
+  // Without mapping to Ints the multiplication happens as chars, which overflows
+  val easyProduct = "Hello".map(Char.char2int).product
+  println(s"Easy Product: ${easyProduct}")
 
-	def recursiveProduct(input: String): Int = {
-		if (input.isEmpty) {
-			1
-		}
-		else {
-			input.head * recursiveProduct(input.tail)
-		}
-	}
-	val helloRecursiveProduct = recursiveProduct("Hello")
-	println(s"Recursive Product: ${helloRecursiveProduct}")
+  def recursiveProduct(input: String): Int = {
+    if (input.isEmpty) {
+      1
+    }
+    else {
+      input.head * recursiveProduct(input.tail)
+    }
+  }
+  val helloRecursiveProduct = recursiveProduct("Hello")
+  println(s"Recursive Product: ${helloRecursiveProduct}")
 
-	def exponent(x: Int, n: Int): Double = {
-		if (n == 0) {
-			1
-		}
-		else if (n < 0) {
-			1 / exponent(x, -n)
-		}
-		else if (n % 2 == 0) {
-			pow(exponent(x, n/2), 2)
-		}
-		else {
-			x * exponent(x, n - 1)
-		}
-	}
-	val twoExponentFour = exponent(2, 4)
-	println(s"Two Exponent Four: ${twoExponentFour}")
-	val hugeExponent = exponent(28, 51)
-	println(s"Huge Exponent: ${hugeExponent}")
-	val twoExponentNegativeFour = exponent(2, -4)
-	println(s"Two Exponent Negative Four: ${twoExponentNegativeFour}")
+  def exponent(x: Int, n: Int): Double = {
+    if (n == 0) {
+      1
+    }
+    else if (n < 0) {
+      1 / exponent(x, -n)
+    }
+    else if (n % 2 == 0) {
+      pow(exponent(x, n/2), 2)
+    }
+    else {
+      x * exponent(x, n - 1)
+    }
+  }
+  val twoExponentFour = exponent(2, 4)
+  println(s"Two Exponent Four: ${twoExponentFour}")
+  val hugeExponent = exponent(28, 51)
+  println(s"Huge Exponent: ${hugeExponent}")
+  val twoExponentNegativeFour = exponent(2, -4)
+  println(s"Two Exponent Negative Four: ${twoExponentNegativeFour}")
 }
