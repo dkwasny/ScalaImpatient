36024ebe6c99380f58ad6f9b49da8e96177e739b
dkwasny <dkwasny67@gmail.com>
2016-09-21 16:19:37 +0000

    Travis test commit

--- src/test/scala/net/kwas/impatient/ch5/CounterTest.scala
+++ src/test/scala/net/kwas/impatient/ch5/CounterTest.scala
@@ -3,7 +3,7 @@ package net.kwas.impatient.ch5
 import org.scalatest._
 
 class CounterTest extends FlatSpec with Matchers {
-  "A Counter" should "be cool" in {
+  "A Counter" should "be coolest" in {
     val counter = new Counter
     counter.current() should be (0)
   }
